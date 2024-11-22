7ab92b2cee1246281534a2a4901350a98a6b94dc
Nettan <jeanette.norberg82@gmail.com>
2024-11-22 11:39:38 +0100

    added gameActivity and rulesFragment and buttons whit some functions

--- app/src/main/java/com/example/cardgame/GuessCardActivity.kt
+++ app/src/main/java/com/example/cardgame/GuessCardActivity.kt
@@ -1,6 +1,5 @@
 package com.example.cardgame
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,20 +7,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class GuessCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_guess_card)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val playGuessCardBtn = findViewById<Button>(R.id.button)
-        playGuessCardBtn.setOnClickListener {
-            val intent = Intent(this,GuessCardActivity::class.java)
-            startActivity(intent)
+        val rulesFragment = RulesFragment()
+        val rulesBtn = findViewById<Button>(R.id.btnRules)
+        rulesBtn.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fr_container,rulesFragment)
+            transaction.commit()
         }
     }
 }
