0565b7ec23fb1fb8c4923aba0e173c184d74ccb0
Nettan <jeanette.norberg82@gmail.com>
2024-11-29 16:18:34 +0100

    added viewModel and random cardfunction in deck class

--- app/src/main/java/com/example/cardgame/Deck.kt
+++ app/src/main/java/com/example/cardgame/Deck.kt
@@ -1,64 +1,78 @@
 package com.example.cardgame
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 class Deck {
 
-    val cards: List<Card> = listOf(
-        Card(1,R.drawable.heartsess),
-        Card(2,R.drawable.hearts2),
-        Card(3,R.drawable.hearts3),
-        Card(4,R.drawable.hearts4),
-        Card(5,R.drawable.hearts5),
-        Card(6,R.drawable.hearts6),
-        Card(7,R.drawable.hearts7),
-        Card(8,R.drawable.hearts8),
-        Card(9,R.drawable.hearts9),
-        Card(10,R.drawable.hearts10),
-        Card(11,R.drawable.heartsjack),
-        Card(12,R.drawable.heartsqueen),
-        Card(13,R.drawable.heartsking),
-        Card(1,R.drawable.diamonsd1),
-        Card(2,R.drawable.diamonds2),
-        Card(3,R.drawable.diamonds3),
-        Card(4,R.drawable.diamonds4),
-        Card(5,R.drawable.diamonds5),
-        Card(6,R.drawable.diamonds6),
-        Card(7,R.drawable.diamonds7),
-        Card(8,R.drawable.diamonds8),
-        Card(9,R.drawable.diamonds9),
-        Card(10,R.drawable.diamonds10),
-        Card(11,R.drawable.diamondsjack),
-        Card(12,R.drawable.diamondsqueen),
-        Card(13,R.drawable.diamondsking),
-        Card(1,R.drawable.clover1),
-        Card(2,R.drawable.clover2),
-        Card(3,R.drawable.clover3),
-        Card(4,R.drawable.clover4),
-        Card(5,R.drawable.clover5),
-        Card(6,R.drawable.clover6),
-        Card(7,R.drawable.clover7),
-        Card(8,R.drawable.clover8),
-        Card(9,R.drawable.clover9),
-        Card(10,R.drawable.clover10),
-        Card(11,R.drawable.cloverjack),
-        Card(12,R.drawable.cloverqueen),
-        Card(13,R.drawable.cloverking),
-        Card(1,R.drawable.spades1),
-        Card(2,R.drawable.spades2),
-        Card(3,R.drawable.spades3),
-        Card(4,R.drawable.spades4),
-        Card(5,R.drawable.spades5),
-        Card(6,R.drawable.spades6),
-        Card(7,R.drawable.spades7),
-        Card(8,R.drawable.spades8),
-        Card(9,R.drawable.spades9),
-        Card(10,R.drawable.spades10),
-        Card(11,R.drawable.spadesjack),
-        Card(12,R.drawable.spadesqueen),
-        Card(13,R.drawable.spadesking),
+   private val _cards: MutableLiveData<MutableList<Card>> = MutableLiveData(mutableListOf())
+    val cards: LiveData<MutableList<Card>> get() = _cards
+    val drawnCards = mutableListOf<Card>()
+    init{
+        createDeck()
+    }
+    fun createDeck(){
+        _cards.value?.add(Card(1,R.drawable.heartsess))
+        _cards.value?.add(Card(2,R.drawable.hearts2))
+        _cards.value?.add(Card(3,R.drawable.hearts3))
+        _cards.value?.add(Card(4,R.drawable.hearts4))
+        _cards.value?.add(Card(5,R.drawable.hearts5))
+        _cards.value?.add(Card(6,R.drawable.hearts6))
+        _cards.value?.add(Card(7,R.drawable.hearts7))
+        _cards.value?.add(Card(8,R.drawable.hearts8))
+        _cards.value?.add(Card(9,R.drawable.hearts9))
+        _cards.value?.add(Card(10,R.drawable.hearts10))
+        _cards.value?.add(Card(11,R.drawable.heartsjack))
+        _cards.value?.add(Card(12,R.drawable.heartsqueen))
+        _cards.value?.add(Card(13,R.drawable.heartsking))
+        _cards.value?.add(Card(1,R.drawable.diamonsd1))
+        _cards.value?.add(Card(2,R.drawable.diamonds2))
+        _cards.value?.add(Card(3,R.drawable.diamonds3))
+        _cards.value?.add(Card(4,R.drawable.diamonds4))
+        _cards.value?.add(Card(5,R.drawable.diamonds5))
+        _cards.value?.add(Card(6,R.drawable.diamonds6))
+        _cards.value?.add(Card(7,R.drawable.diamonds7))
+        _cards.value?.add(Card(8,R.drawable.diamonds8))
+        _cards.value?.add(Card(9,R.drawable.diamonds9))
+        _cards.value?.add(Card(10,R.drawable.diamonds10))
+        _cards.value?.add(Card(11,R.drawable.diamondsjack))
+        _cards.value?.add(Card(12,R.drawable.diamondsqueen))
+        _cards.value?.add(Card(13,R.drawable.diamondsking))
+        _cards.value?.add(Card(1,R.drawable.clover1))
+        _cards.value?.add(Card(2,R.drawable.clover2))
+        _cards.value?.add(Card(3,R.drawable.clover3))
+        _cards.value?.add(Card(4,R.drawable.clover4))
+        _cards.value?.add(Card(5,R.drawable.clover5))
+        _cards.value?.add(Card(6,R.drawable.clover6))
+        _cards.value?.add(Card(7,R.drawable.clover7))
+        _cards.value?.add(Card(8,R.drawable.clover8))
+        _cards.value?.add(Card(9,R.drawable.clover9))
+        _cards.value?.add(Card(10,R.drawable.clover10))
+        _cards.value?.add(Card(11,R.drawable.cloverjack))
+        _cards.value?.add(Card(12,R.drawable.cloverqueen))
+        _cards.value?.add(Card(13,R.drawable.cloverking))
+                _cards.value?.add(Card(1,R.drawable.spades1))
+        _cards.value?.add(Card(2,R.drawable.spades2))
+        _cards.value?.add(Card(3,R.drawable.spades3))
+        _cards.value?.add(Card(4,R.drawable.spades4))
+        _cards.value?.add(Card(5,R.drawable.spades5))
+        _cards.value?.add(Card(6,R.drawable.spades6))
+        _cards.value?.add(Card(7,R.drawable.spades7))
+        _cards.value?.add(Card(8,R.drawable.spades8))
+        _cards.value?.add(Card(9,R.drawable.spades9))
+        _cards.value?.add(Card(10,R.drawable.spades10))
+        _cards.value?.add(Card(11,R.drawable.spadesjack))
+        _cards.value?.add(Card(12,R.drawable.spadesqueen))
+        _cards.value?.add(Card(13,R.drawable.spadesking))
+    }
 
-        )
-    fun showRandomCard():Card{
-        return cards.random()
 
+    fun drawRandomCard(): Card? {
+        val avalibleCards = _cards.value?.filter {it !in drawnCards}
+        val card = avalibleCards?.randomOrNull()
+        if( card != null){
+            drawnCards.add(card)
+        }
+        return card
     }
 }
--- app/src/main/java/com/example/cardgame/Player1Fragment.kt
+++ app/src/main/java/com/example/cardgame/Player1Fragment.kt
@@ -8,12 +8,13 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+
+
 import com.example.cardgame.databinding.FragmentPlayer1Binding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -21,20 +22,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Player1Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
+
     private var _binding:FragmentPlayer1Binding?=null
     private val binding get() = _binding!!
-        private lateinit var deck: Deck
-    private var param1: String? = null
-    private var param2: String? = null
+    lateinit var vm: SharedViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
 
@@ -42,18 +35,19 @@ class Player1Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentPlayer1Binding.inflate(inflater,container,false)
-        val view = inflater.inflate(R.layout.fragment_player1,container,false)
-        val playerGuess = view.findViewById<EditText>(R.id.yourGuess_et)
+        vm = ViewModelProvider(requireActivity()).get(SharedViewModel::class.java)
 
-        val showCard = view.findViewById<ImageView>(R.id.show_card)
-        val pointView = view.findViewById<TextView>(R.id.tv_showPoints)
         val hihger = 0
         val points = 0
-        deck = (activity as GameActivity).deck
-        @Suppress("DEPRECATION")
-        val card:Card? = arguments?.getParcelable("selectedCard")
+
        binding.guessButton.setOnClickListener {
-            //showCard.setImageResource(card?.value? ,card.imageResId?)
+           vm.drawRandomCard()
+           vm.selectedCard.observe(viewLifecycleOwner) { selectedCard ->
+               // Hämta och visa slumpmässigt kort
+               if (selectedCard != null) {
+                   binding.showCard.setImageResource(selectedCard.imageResId)
+               }
+           }
         }
 
 
@@ -65,23 +59,5 @@ class Player1Fragment : Fragment() {
         _binding = null
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Player1Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Player1Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
